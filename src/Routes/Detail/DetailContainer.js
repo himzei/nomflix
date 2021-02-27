2c038b29b4734544e8032f34a880ccb87cc55458
unknown <himzei@gmail.com>
2021-02-27 13:27:59 +0900

    init

---
+++ src/Routes/Detail/DetailContainer.js
@@ -0,0 +1,54 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { movieApi, tvApi } from "api";
+import React from "react";
+import DetailPresenter from "./DetailPresenter";
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/"),
+    };
+  }
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push },
+    } = this.props;
+    const { isMovie } = this.state;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push("/");
+    }
+    let result;
+    try {
+      if(isMovie){
+        ({data: result} = await movieApi.movieDetail(parsedId)) 
+        
+      }else{
+        ({data: result} = await tvApi.showDetail(parsedId)) 
+        
+      }
+      console.log(result)
+    } catch {
+      this.setState({ error: "Can't find anything" });
+    } finally {
+      this.setState({ loading: false, result });
+    }
+  }
+
+  render() {
+    const { result, loading, error } = this.state;
+    console.log(this.state)
+    return <DetailPresenter result={result} loading={loading} error={error} />;
+  }
+}
---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,70 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { movieApi, tvApi } from "api";
+import React from "react";
+import SearchPresenter from "./SearchPresenter";
+
+export default class extends React.Component {
+  state = {
+    movieResults: null,
+    tvResults: null,
+    searchTerm: "",
+    loading: false,
+    error: null,
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchByTerm(searchTerm);
+    }
+  };
+
+  updateTerm = (event) => {
+    const {
+      target: { value },
+    } = event;
+    
+    this.setState({
+      searchTerm: value,
+    });
+  };
+
+  searchByTerm = async (term) => {
+    const { searchTerm } = this.state;
+    this.setState({ loading: true });
+    try {
+      const {
+        data: { results: movieResults },
+      } = await movieApi.search(searchTerm);
+      const {
+        data: { results: tvResults },
+      } = await tvApi.search(searchTerm);
+      this.setState({
+        movieResults,
+        tvResults,
+      });
+      this.setState({ loading: true });
+    } catch {
+      this.setState({ error: "Can't find results" });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const { movieResults, tvResults, searchTerm, loading, error } = this.state;
+
+    return (
+      <SearchPresenter
+        movieResults={movieResults}
+        tvResults={tvResults}
+        searchTerm={searchTerm}
+        loading={loading}
+        error={error}
+        handleSubmit={this.handleSubmit}
+        updateTerm={this.updateTerm}
+      />
+    );
+  }
+}
